e476137bd3201f4acfcada5f100fb008f1edfb67
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-03-18 16:46:02 +0000

    Add sticky navbar feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 
 const navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
             <Link className="navbar-brand" to='/'>CyberSparrow</Link>
             <button className="navbar-toggler"
