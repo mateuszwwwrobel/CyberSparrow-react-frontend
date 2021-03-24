4d0feba519079a77b29ac0eb457e18d969f849b9
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-03-24 10:29:45 +0000

    Fix Link redirects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,17 +9,17 @@ const Footer = () => {
         <div className="text-dark text-center bg-yellow mt-5 pt-3">
             <div className='row'>
                 <div className="col-12">
-                    <Link target="_blank" to="https://www.facebook.com/mateusz.wrobel.796" className="mx-3 text-navy">
+                    <Link target="_blank" to={{pathname: "https://www.facebook.com/mateusz.wrobel.796", state: { fromDashboard: true }}} className="mx-3 text-navy">
                         <FontAwesomeIcon icon={faFacebook} size="lg" transform="grow-4" />
                     </Link>
-                    <Link target="_blank" to="https://github.com/mateuszwwwrobel" className="text-navy mx-3">
+                    <Link target="_blank" to={{pathname: "https://github.com/mateuszwwwrobel", state: { fromDashboard: true }}} className="text-navy mx-3">
                         <FontAwesomeIcon icon={faGithub} size="lg" transform="grow-4" />
                     </Link>
-                    <Link target="_blank" to="https://www.youtube.com/channel/UCqbh_cQEJo81E6RvjHllU5w/videos"
+                    <Link target="_blank" to={{pathname: "https://www.youtube.com/channel/UCqbh_cQEJo81E6RvjHllU5w/videos" , state: { fromDashboard: true }}}
                           className="text-navy mx-3">
                         <FontAwesomeIcon icon={faYoutube} size="lg" transform="grow-4" />
                     </Link>
-                    <Link target="_blank" to="https://www.linkedin.com/in/mateusz-wrobel/" className="mx-3 text-navy">
+                    <Link target="_blank" to={{pathname: "https://www.linkedin.com/in/mateusz-wrobel/" , state: { fromDashboard: true }}} className="mx-3 text-navy">
                         <FontAwesomeIcon icon={faLinkedin} size="lg" transform="grow-4" />
                     </Link>
                 </div>
